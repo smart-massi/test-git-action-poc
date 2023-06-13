84144741bc67b7c41dbc7d4d8f7d616e10f83ec5
Ismail bennis <ismail.bennis@smart-chain.fr>
2023-06-13 17:14:38 +0200

    :sparkles: adding user page

--- application/src/App.js
+++ application/src/App.js
@@ -2,8 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route } from "react-router-dom";
 import User from "./User";
+import React from "react";
 
 function App() {
+	const UserWrapper = () => {
+		const match = React.useMatch(); // i.e. match.params
+
+		return <User match={match} />; // i.e. this.props.match.params
+	};
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -11,7 +17,7 @@ function App() {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<Route path="/user/:id" element={<User />} />
+				<Route path="/user/:id" element={<UserWrapper />} />
 			</header>
 		</div>
 	);
